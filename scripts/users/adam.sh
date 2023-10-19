e3e72c35701c6ffce1c259be7dc8f0a109036e27
AdamPettinger <adam.l.pettinger@gmail.com>
2023-10-19 15:08:55 -0500

    Change github email

--- scripts/users/adam.sh
+++ scripts/users/adam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # An alias for changing the git user on this computer
-alias git_adam='git config --global user.name AdamPettinger ; git config --global user.email adam.pettinger@utexas.edu'
+alias git_adam='git config --global user.name AdamPettinger ; git config --global user.email adam.l.pettinger@gmail.com'
 
 #
 #    Git
